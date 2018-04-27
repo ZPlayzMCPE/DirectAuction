2b12c3d85d31b0e8d894c086d02007003b80475e
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-27 10:42:32 +0100

    Update DirectAuction.php

--- src/semteul/directauction/DirectAuction.php
+++ src/semteul/directauction/DirectAuction.php
@@ -620,11 +620,11 @@ class DirectAuction extends PluginBase implements Listener {
 		if($item->hasEnchantments()) {
 			$effects = $item->getEnchantments();
 			$effectStr = "";
-			foreach($effects as $enchantmentInstance => $enchantment) {
+			foreach($effects as $enchantmentInstance) {
 				if($effectStr !== "") {
 					$effectStr .= ", ";
 				}
-				$effectStr .= $enchantment->getName() . " " . $enchantmentInstance->getLevel();
+				$effectStr .= $enchantmentInstance->getName() . " " . $enchantmentInstance->getLevel();
 			}
 			return $effectStr;
 		}else {
